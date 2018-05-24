hectorchu/litecoin

commit ab87c0b7a40c5d9137117764da0731444604aead

break circular dependency: random/sync -> util -> random/sync

src/random.cpp
@@ -11,14 +11,15 @@
 #include <compat.h> // for Windows API
 #include <wincrypt.h>
 #endif
-#include <util.h>             // for LogPrint()
-#include <utilstrencodings.h> // for GetTime()
+#include <logging.h>  // for LogPrint()
+#include <utiltime.h> // for GetTime()
 
 #include <stdlib.h>
 #include <chrono>
 #include <thread>
 
 #ifndef WIN32
+#include <fcntl.h>
 #include <sys/time.h>
 #endif
 

src/sync.cpp
@@ -4,12 +4,14 @@
 
 #include <sync.h>
 
-#include <memory>
-#include <set>
-#include <util.h>
+#include <logging.h>
 #include <utilstrencodings.h>
 
 #include <stdio.h>
+
+#include <map>
+#include <memory>
+#include <set>
 
 #ifdef DEBUG_LOCKCONTENTION
 #if !defined(HAVE_THREAD_LOCAL)

commit d6db268f1cb740fd3ae47e545bd91151124dc99c

Fix FreeBSD build by including utilstrencodings.h
`random.cpp` needs to explicitly include `utilstrencodings.h` to get
`ARRAYLEN`. This fixes the FreeBSD build.

This was broken in ab87c0b7a40c5d9137117764da0731444604aead.

src/random.cpp
@@ -34,6 +34,7 @@
 #include <sys/random.h>
 #endif
 #ifdef HAVE_SYSCTL_ARND
+#include <utilstrencodings.h> // for ARRAYLEN
 #include <sys/sysctl.h>
 #endif
 